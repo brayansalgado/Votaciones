189887176bec5cc3a6b5e9c7e0a62e18c20132a6
brayansmith <brayansmith@SILVER-PC>
2017-05-31 00:02:56 -0500

    CrudEstado

---
+++ Votaciones/Models/Estado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Votaciones.Models
+{
+    public class Estado
+    {
+        [Key]
+        public int idEstado { get; set; }
+        public String descripcion { get; set; }
+
+    }
+}
---
+++ Votaciones/Models/VotacionesContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Votaciones.Models
+{
+    public class VotacionesContext:DbContext
+    {
+        public VotacionesContext() : base("DefaultConnection")
+        {
+    }
+        public DbSet<Estado> Estados { get; set; }
+    }
+}
